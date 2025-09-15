2e3b9464153934469a69ff97e11e164990fb224f
あなたの名前 <>
2025-09-16 04:25:21 +0900

    game-page(仮)実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,12 @@ class GamesController < ApplicationController
   end
 
   def show
+  @clicks = params[:clicks].to_i
+
+  @result = case @clicks
+            when 0..9 then "タンクトップ👕"
+            when 10..19 then "セーター🧶"
+            else "コート🧥"
+            end
   end
 end
