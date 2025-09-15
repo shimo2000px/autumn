768b219cde5c7304bc7f99f205e85b23130f17a4
あなたの名前 <>
2025-09-15 22:45:59 +0900

    Fix:rubocop修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root "top#index"
-  #get "up" => "rails/health#show", as: :rails_health_check
-
-
+  # get "up" => "rails/health#show", as: :rails_health_check
 end
