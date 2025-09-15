e21ee4d5836d36d9da54f8ce9dc75bc0c7da3630
あなたの名前 <>
2025-09-16 01:51:15 +0900

    usageページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root "top#index"
+  root 'top#index'
+  get 'games', to: 'games#show'
+  get 'result', to: 'results#show'
+  get 'usage', to: 'usage#show'
+
   # get "up" => "rails/health#show", as: :rails_health_check
 end
