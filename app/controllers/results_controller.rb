02698b85b2de0118527830fabf6460bf8ebd2c64
あなたの名前 <>
2025-09-16 15:50:54 +0900

    result-pageレイアウト完了

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,10 +2,10 @@ class ResultsController < ApplicationController
   def show
     @clicks = (params[:clicks] || 0).to_i
 
-    if @clicks >= 75
+    if @clicks >= 70
       render :super
     elsif @clicks >= 60
-      render :great
+      render :greate
     elsif @clicks >= 50
       render :good
     elsif @clicks >= 40
